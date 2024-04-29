42849812fa37d0c6edf9ad85f6cee46ae278e9c6
Dhanvin Lakshmisha <dhanboss01@gmail.com>
2024-04-29 08:46:58 -0500

    skeleton of square

--- WumpusSquare.java
+++ WumpusSquare.java
@@ -22,9 +22,35 @@ public class WumpusSquare
         this.stench = false;
         this.visited = new boolean[10][10];
     }
-    //add get for each attribute
 
-    //add set for each attribute
+    public boolean getGold()
+    {
+        return gold;
+    }
+    public boolean getLadder()
+    {
+        return ladder;
+    }
+    public boolean getPit()
+    {
+        return pit;
+    }
+    public boolean getBreeze()
+    {
+        return breeze;
+    }
+    public boolean getWumpus()
+    {
+        return wumpus;
+    }
+    public boolean getDeadWumpus()
+    {
+        return deadWumpus;
+    }
+    public boolean getStench()
+    {
+        return stench;
+    }
 
     public String toString()
     {
