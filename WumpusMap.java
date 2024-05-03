9985af5a5368456f21f32ff2962a0d9c5d37c2f1
Dhanvin Lakshmisha <dhanboss01@gmail.com>
2024-05-03 08:41:21 -0500

    finished createMap()

--- WumpusMap.java
+++ WumpusMap.java
@@ -18,6 +18,14 @@ public class WumpusMap
     {
         //1 is pit
         //2 is breeze
+        //set all the squares to a wumpus square
+        for(int x= 0;x<grid.length;x++)
+        {
+            for(int y =0;y<grid[0].length;y++)
+            {
+                grid[x][y] = new WumpusSquare();
+            }
+        }
         ArrayList<Integer> xUsed = new ArrayList<>();
         ArrayList<Integer> yUsed = new ArrayList<>();
         int randX = 0;
@@ -33,23 +41,65 @@ public class WumpusMap
         {
             randX = 0 + (int)(Math.random() * ((9 - 0) + 1));
             randY = 0 + (int)(Math.random() * ((9 - 0) + 1));
-            while(!xUsed.contains(randX) && !yUsed.contains(randY))
+            while(xUsed.contains(randX) && yUsed.contains(randY))
             {
                 randX = 0 + (int)(Math.random() * ((9 - 0) + 1));
                 randY = 0 + (int)(Math.random() * ((9 - 0) + 1));
             }
             xUsed.add(randX);
             yUsed.add(randY);
-            grid[randX][randY] = new WumpusSquare();
             grid[randX][randY].setPit(true);
         }
 
         //adding breezes
         for(int i =0;i< grid.length;i++)
         {
-            for(int k = 0;k)
-        }
+            for(int k = 0;k<grid[0].length;k++)
+            {
 
+                    if(grid[i][k].getPit())
+                    {
+                        if(i>0)
+                        {
+                            grid[i-1][k].setBreeze(true);
+                        }
+                        if(i<grid.length-1)
+                        {
+                            grid[i+1][k].setBreeze(true);
+                        }
+                        if(k>0)
+                        {
+                            grid[i][k-1].setBreeze(true);
+                        }
+                        if(k< grid.length-1)
+                        {
+                            grid[i][k+1].setBreeze(true);
+                        }
+                    }
+
+            }
+        }
+        //adding the gold
+        int randX1 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+        int randY1 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+        while(grid[randX1][randY1].getPit() && grid[randX1][randY1].getLadder())
+        {
+            randX1 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+            randY1 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+        }
+        grid[randX1][randY1].setGold(true);
+
+        //adding ladder
+        int randX2 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+        int randY2 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+        while(grid[randX2][randY2].getPit() && grid[randX2][randY2].getLadder() && grid[randX2][randY2].getGold())
+        {
+            randX2 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+            randY2 = 0 + (int)(Math.random() * ((9 - 0) + 1));
+        }
+        grid[randX2][randY2].setLadder(true);
+        ladderC = randY2;
+        ladderR = randX2;
 
     }
     public int getLadderC()
